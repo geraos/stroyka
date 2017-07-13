8c2ac9e140b3e1fdd091f7d102884da8719103a0
Alex Posseda <alexposseda@gmail.com>
2017-07-13 18:57:58 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var connect = require('gulp-connect');
+var htmlmin = require('gulp-htmlmin');
+var htmlincluder = require('gulp-htmlincluder');
+var less = require('gulp-less');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('server', function(){
+    connect.server({
+        root: 'build/',
+        livereload: true
+    });
+});
+
+gulp.task('html', function(){
+    gulp.src('dev/**/*.html')
+        .pipe(htmlincluder())
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeComments: 1
+        }))
+        .pipe(rename(function(path){
+            path.dirname = ''
+        }))
+        .pipe(gulp.dest('build/'))
+        .pipe(connect.reload());
+});
+
+gulp.task('css', function(){
+    gulp.src('dev/assets/less/*.less')
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('build/css/'))
+        .pipe(connect.reload());
+});
+
+gulp.task('move', function(){
+    gulp.src('dev/assets/img/*.*')
+        .pipe(rename(function(path){
+            path.dirname = ''
+        }))
+        .pipe(gulp.dest('build/img/'));
+    gulp.src('dev/assets/fonts/*.*')
+        .pipe(gulp.dest('build/fonts/'));
+});
+
+gulp.task('default', function(){
+    gulp.start(['server', 'move', 'html', 'css']);
+
+    gulp.watch(['dev/**/*.html'], function(){
+        gulp.start(['html']);
+    });
+
+    gulp.watch(['dev/**/*.less'], function(){
+        gulp.start(['css']);
+    });
+});
